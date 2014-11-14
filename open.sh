4debf6ad87f3003b1a029980a5f64f0f222174d6
n0b1dy <n0b1dy@users.noreply.github.com>
2014-11-14 16:25:52 +0000

    Update open.sh

--- open.sh
+++ open.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # by n0b1dy
+# get as an input .nmap scan and gives the uniq open ports that are open
 cat $1 | grep "open" | cut -d/ -f1 | grep -v "Warning" | sort -n | uniq > scan.tcp.open

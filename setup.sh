0cf50d203a04e1ffa95cef13e5e6481fb7a3b78e
n0b1dy <n0b1dy@users.noreply.github.com>
2016-07-03 19:58:31 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -356,6 +356,7 @@ whitelist_bypass()
   git clone https://github.com/fdiskyou/PowerOPS/
   git clone https://github.com/Cn33liz/p0wnedShell
   git clone https://github.com/Cn33liz/SharpCat
+  git clone https://github.com/khr0x40sh/WhiteListEvasion
 }
 
 post_exploit()

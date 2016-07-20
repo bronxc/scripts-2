3156bd613558793f84a729633ac5ce9ec87ca7ac
n0b1dy <n0b1dy@users.noreply.github.com>
2016-07-20 12:36:08 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -464,6 +464,7 @@ git clone https://github.com/subTee/PoshRat.git
 git clone https://github.com/giMini/RWMC
 git clone https://github.com/PowerShellEmpire/Empire.git
 git clone https://github.com/NetSPI/PowerUpSQL
+git clone https://github.com/leechristensen/UnmanagedPowerShell
 cd ..
 mkdir cobalt
 git clone https://github.com/rvrsh3ll/POSH-Commander

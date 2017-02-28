a49dc31314604db1ff0ddd9fafc0b494f4cce431
n0b1dy <n0b1dy@users.noreply.github.com>
2017-02-28 14:37:23 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -580,6 +580,7 @@ phishing()
 	clone https://cybersyndicates.com/project/SimplyTemplate/
 	clone https://github.com/Section9Labs/Cartero
 	clone https://gist.github.com/monoxgas/7fec9ec0f3ab405773fc
+	clone https://github.com/Mr-Un1k0d3r/CatMyFish
 	cd ..
 	cd $CURRENT
 }
